c3392cfad26414f3705036d7520f6ba50e706546
MKBob <mkhalilbob@gmail.com>
2025-11-19 19:12:53 +0500

    file-update-two

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import logo from "../components/images/logo.svg"
+import logo from "../components/images/logo.png"
 
 const Footer = () => {
     return (
-        <div>
-         
-            <p className='text-5xl font-bold flex items-center'>More than 25,000 teams use Collabs</p>
-
-            <div className='flex items-center justify-between'>
-                <img src={logo} alt='group' />
-
-            </div>
-         
-           
+        <div className='flex flex-col items-center justify-center text-center space-y-6'>
+            <p className='text-5xl font-bold pt-15'>More than 25,000 teams use Collabs</p>
+            <img src={logo} alt='group' className='mx-auto py-10' />
         </div>
     )
 }
